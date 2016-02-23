9d0af20610d5e862ed011997186d75aa882396ff
hollm164 <marielleholland@marielles-mbp.wireless.newschool.edu>
2016-02-23 17:34:53 -0500

    done!

--- sketch.js
+++ sketch.js
@@ -1,13 +1,52 @@
 //Feel free to change these words:
-var nouns = ["Hammer","Cat","Tree", "Silence", "Umbrella","Sofa","Shirt","Jacket"];
+var nouns = ["HAMMER","PIG","TREE","SILENCE", "ROSE","COFFEE","CLOVER","CARDBOARD"];
 
+var randNum, randNoun;
+
+var articles = ["THE","A"];
+
+var randArticle;
+
+var randNum2;
+
+var adjectives = ["BLUE","SOFT","SHINY","COLORFUL","FUZZY","BEAUTIFUL", "UGLY"];
+
+var randAdj;
+
+var randNum3;
+
+var randColor;
+
+var randColor2;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(800,800);
+
+	randNum = floor (random(0,nouns.length));
+	randNum2 = floor (random(0,articles.length));
+	randNum3 = floor (random(0,adjectives.length));
+
+	randNoun = nouns[randNum];
+	randArticle = articles[randNum2];
+	randAdj = adjectives[randNum3];
 	
+	randColor = color(random(255),random(255),random(255));
+	randColor2 = color(random(255),random(255), random(255));
+
 }
 
 function draw(){
-	background("silver");
+	background(randColor);
+
+	for(var i=0; i<10; i++){
+		fill(randColor2);
+		noStroke();
+		ellipse(100*i,100*i,20,520);
+	}
+
+	textFont("Bebas Kai");
+	textSize(100);
+	fill("white");
+	text(randArticle+" "+randAdj+" "+randNoun,25,400);
 
 }
